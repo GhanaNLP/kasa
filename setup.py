8e91f5ed615382126499bea463188e060858a6a4
hotlynn2 <laizer421@gmail.com>
2021-03-08 00:10:00 +0000

    commit

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from distutils.core import setup
 setup(name='Kasa',
     version='0.0.1',
     description='A library of translation and other NLP tools for the Ghanaian language Twi',
+    author='Dr.Paul Azunre',
+    author_email='azunre@gmail.com',
+    license='MIT',
     packages=['Kasa'],
-    install_requires=['gensim >= 3.8.1'],
+    install_requires=['gensim == 3.8.1'],
     include_package_data=True,
 )
+ 
+
