a7ac8ed6927329efebf282b403730c810f02728c
hotlynn2 <laizer421@gmail.com>
2021-03-21 00:29:11 +0000

    commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Kasa',
-    version='0.0.1',
+    version='0.0.2',
     description='A library of translation and other NLP tools for the Ghanaian language Twi',
     author='Dr.Paul Azunre',
     author_email='azunre@gmail.com',
