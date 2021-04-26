75443b842ce618093d2204e83e841e06bfd89b17
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-04-26 23:47:51 +0000

    setup.py requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='Kasa',
     author_email='azunre@gmail.com',
     license='MIT',
     packages=['Kasa'],
-    install_requires=['gensim == 3.8.1' , 'transformers', 'tokenizers==0.10.2'],
+    install_requires=['gensim == 3.8.1' , 'tokenizers==0.10.2'],
     include_package_data=True
 )
  
