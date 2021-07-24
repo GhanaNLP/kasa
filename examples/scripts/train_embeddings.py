279714cafe53bed7163adc5a2b69dd2c72334453
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-07-24 14:54:16 +0000

    directory change

--- examples/scripts/train_embeddings.py
+++ examples/scripts/train_embeddings.py
@@ -36,8 +36,8 @@ args = parser.parse_args()
 
 NUMBER_OF_DATASET = 100
 DIMENSION = 300
-DATA_DIR = "..\..\data"
-MODELS_DIR = "..\..\models"
+DATA_DIR = "..\data"
+MODELS_DIR = "..\models"
 TWI_PATH = args.data if args.data else os.path.join(DATA_DIR, "jw300.en-tw.tw")
 WORDSIM_PATH = os.path.join(DATA_DIR, "wordsim_tw.csv")
 
