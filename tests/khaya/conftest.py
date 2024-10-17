19b46b2b87605cb1910e8f64797d561f82724d86
Lawrence Mac <bh-204@hotmail.com>
2024-10-17 23:25:01 +0200

    update poetry and tests

--- tests/khaya/conftest.py
+++ tests/khaya/conftest.py
@@ -1,16 +1,19 @@
 import os
 
 import pytest
+from dotenv import load_dotenv
 
 from khaya.khaya_interface import KhayaInterface
 
-khaya_api_key = os.getenv("khaya_api_key", "test_key")
+load_dotenv()
+khaya_api_key = os.getenv("khaya_api_key")
 
 
 @pytest.fixture
 def khaya_interface():
     api_key = khaya_api_key
     return KhayaInterface(api_key)
+
 
 # @pytest.fixture
 # def audio_file():
