3b50a30df32e21b8e66327198da975ff5e16a85e
Lawrence Adu-Gyamfi <bh-204@hotmail.com>
2024-10-18 10:47:16 +0200

    [FIX] update conftests for api_key management

--- tests/khaya/conftest.py
+++ tests/khaya/conftest.py
@@ -5,19 +5,13 @@ from dotenv import load_dotenv
 
 from khaya.khaya_interface import KhayaInterface
 
+os.environ.pop("khaya_api_key", None)  # Remove the key if it exists
+
 load_dotenv()
-khaya_api_key = os.getenv("khaya_api_key")
+khaya_api_key = os.getenv("khaya_api_key", None)
 
 
 @pytest.fixture
 def khaya_interface():
     api_key = khaya_api_key
     return KhayaInterface(api_key)
-
-
-# @pytest.fixture
-# def audio_file():
-
-#     audio_file_path = "tests/khaya/me_ho_ye.wav"
-
-#     audio_data, sampling_rate
