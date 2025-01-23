76ce4a8dfb2a211beaba567ed628687e40037395
~pkwolffe <116769499+PhidLarkson@users.noreply.github.com>
2025-01-23 11:00:20 +0000

    Create preprocessor.py

---
+++ khaya/preprocessor.py
@@ -0,0 +1,10 @@
+from num2words import num2words
+import re
+
+# function to deal with digit to words conversion of corresponding numbers
+def convert(text):
+    def replace(match):
+        return num2words(int(match.group()))
+    
+    return re.sub(r'\b\d+\b', replace_number, text)
+
